8404d5fd0903d198847c78d92a540a757fee0a4d
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2020-06-05 18:00:51 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,18 +4,49 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Ok</Text>
+      <View style={styles.container1}>
+        <Text>Cyan</Text>
+      </View>
+      <View style={styles.container2}>
+        <Text>Blue</Text>
+      </View>
+      <View style={styles.container3}>
+        <Text>Magenta</Text>
+      </View>
+      <View style={styles.container4}>
+        <Text>Orange</Text>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#385495',
+  container1: {
+    backgroundColor: '#2aa198',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    padding: 10,
+  },
+  container2: {
+    backgroundColor: '#268bd2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    padding: 10,
+  },
+  container3: {
+    backgroundColor: '#d33682',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    padding: 10,
+  },
+  container4: {
+    backgroundColor: '#cb4b16',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    padding: 10,
   },
 });
