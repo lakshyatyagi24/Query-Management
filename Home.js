d576f499f45cbeed5b17e803970d4a0b9e068e09
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2020-07-13 18:56:43 +0530

    .

--- Home.js
+++ Home.js
@@ -24,6 +24,7 @@ class Home extends Component {
                     barStyle="dark-content"
                 />
                 <Text>Hi</Text>
+                
             </View>
         );
     }
